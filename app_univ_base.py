7f288cbe776e5c7607169d71faaccad445e079e4
LiliyaOtchenash <olm@europe.com>
2015-08-31 14:33:30 +0300

    teacher name

--- app_univ_base.py
+++ app_univ_base.py
@@ -2,29 +2,11 @@ from pymongo import MongoClient
 client = MongoClient()
 db = client.univ_database
 
-collection_student = db.collection_student
-Liliya = {'name': 'Liliya', 'sourname': 'Otchenash', 'age': 27, 'groop': 1, 'specialty': 'web-development', 'course': 'Python'}
-collection_student_id = collection_student.insert_one(Liliya).inserted_id  ###  create id
-print(collection_student_id)
-
-
-collection_teacher = db.collection_teacher
-Andrew = {'name': 'Andrew', 'sourname': 'Pogrebnjak', 'age': 27, 'experience': '10_years', 'teacher_direction': 'web-development', 'course': ['Python', 'JScript']}
-collection_teacher_id = collection_teacher.insert_one(Andrew).inserted_id ###
-print(collection_teacher_id)
-
-
-collection_course = db.collection_course
-Pythons = {'name': 'Pythons', 'last_version': '3.4', 'course_duration': '6_month', 'teacher': 'Andrew'}
-collection_course_id = collection_course.insert_one(Pythons).inserted_id
-print(collection_course_id)
-Jscript = {'name': 'JScript', 'last_version': '5.4', 'course_duration': '2_month', 'teacher': 'Andrew'}
-db.collection_course.save(Jscript)
 
 for docs in db.collection_course.find():
     print('list of docs', docs)
 
-
+print('1', list(db.collection_teachern_.find()))
 
 print(db.collection_names(include_system_collections=False))
 print(db)
--- doc_list.py
+++ doc_list.py
@@ -1,24 +1,24 @@
 __author__ = 'a'
+from pymongo import MongoClient
+client = MongoClient()
+db = client.univ_database
 
-course1 = {'title': 'Python',
-          'description': 'widely used general-purpose, high-level programming language'}
-course2 = {'title': 'JavaScript',
-           'description': 'high level, dynamic, untyped, and interpreted programming language'}
-course3 = {'title': 'Java',
-           'description': 'general-purpose computer programming language' }
-
-
-teacher1 = {'first_name': 'Andrew', 'cource_id': course1.get('_id')}
-teacher2 = {'first_name': 'Anton', 'cource_id': course2.get('_id')}
-teacher3 = {'first_name': 'Ivan', 'cource_id': course3.get('_id')}
+print(list(db.course.find()))
+print(list(db.teacher.find()))
+print(list(db.student.find()))
 
 
-student1 = {'name': 'Liliya', 'sourname': 'Otchenash', 'groop': 1,
-            'course_id': [course1.get('_id'), course2.get('_id')]}
-student2 = {'name': 'Olga', 'sourname': 'Rudenko', 'groop': 2,
-            'course_id': [course1.get('_id'), course3.get('_id')]}
-student3 = {'name': 'Fill', 'sourname': 'Sharp', 'groop': 1,
-            'course_id': [course2.get('_id'), course3.get('_id')]}
+
+db_teacher_first_name = db.teacher.find({}, {'first_name': 1, '_id': 0})
+print('TEACHER NAME')
+for teacher in list(db_teacher_first_name):
+    #print('TEACHERS: ', teacher)
+    print(list(teacher.values()))
+
+
+
+#db.teacher.find('null', {'first_name': 1, '_id': 0})
+
 
 
 
