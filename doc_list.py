1b89688dded308bf7e84a2d52a70a4ce3ecaaf71
LiliyaOtchenash <olm@europe.com>
2015-08-31 18:42:40 +0300

    courses name

--- doc_list.py
+++ doc_list.py
@@ -2,16 +2,46 @@ from pymongo import MongoClient
 client = MongoClient()
 db = client.univ_database
 db_teacher_first_name = db.teacher.find({}, {'first_name': 1, '_id': 0})
+db_course_title = db.course.find({}, {'title': 1, '_id': 0})
+
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--courses', '-c', help='list of courses', action='store_true')
+parser.add_argument('--teachers', '-t', help='list of courses', action='store_true')
+parser.add_argument('--courses', '-c', help='list of courses and teachers', action='store_true')
+
 args = parser.parse_args()
 
-if args.courses:
+if args.teachers:
     print('TEACHER NAME')
     for teacher in list(db_teacher_first_name):
-        print(list(teacher.values()))
+        print(list(teacher.values())[0])
+
+
+if args.courses:
+    print('course name', 'teacher name')
+    for course in list(db_course_title):
+        cors = list(course.values()[0]
+        for teacher in list(db_teacher_first_name):
+            print(cors, list(teacher.values())
+
+
+
+
+
+print(cor)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
